e9ab1b89e2cbe7f52d61afd2d65c8fe20e36da87
Massipou <vivxxl@gmail.com>
2022-05-18 16:53:49 +0200

    supprimé :        AutoZilla/config/hooks/live/0100-keyboard-config.hook.chroot 	nouveau fichier : AutoZilla/config/hooks/live/0105-keyboard-config.hook.chroot 	-	changing keyboard layout at build time with nano on /etc/default/keyboard
    	modifié :         AutoZilla/config/includes.chroot_after_packages/etc/profile.d/launcher.sh
    	modifié :         AutoZilla/config/includes.chroot_after_packages/script/launch.sh
    	-	trying to ping CIFS server before starting

--- AutoZilla/config/includes.chroot_after_packages/etc/profile.d/launcher.sh
+++ AutoZilla/config/includes.chroot_after_packages/etc/profile.d/launcher.sh
@@ -1,16 +1,3 @@
-start="true"
-
-while [ start = "true" ]
-do
-	echo -e "\nChecking Network Connexion ...\n"
-	sudo dhclient;
-	dhcplease=$(sudo grep dhclient /var/log/syslog | grep "DHCPACK")
-	if [ $dhcplease != "" ]; then
-		echo -e "\nFind Network Connexion !!!\n"
-		start="false"
-	else
-		echo -e "\nFail Network Connexion ...\nRetrying ...\n"
-	fi
-done
-
+sudo dhclient;
+sleep 2.5
 sudo bash /script/launch.sh
--- AutoZilla/config/includes.chroot_after_packages/script/launch.sh
+++ AutoZilla/config/includes.chroot_after_packages/script/launch.sh
@@ -7,21 +7,18 @@ sudo umount /home/partimag/
 
 sudo touch /script/logs.txt
 
+
+
 cp /lib/live/mount/medium/preconfig.cfg .
 . preconfig.cfg
 
-#echo "connecting to the network ..." > /script/logs.txt
+echo -e "\n Trying connection to share IP \n"
 
-#dhtrying=1 # nothing = win / else = fail
-
-#while [ dhtrying != ""]
-#do
-#	sudo dhclient;
-#	dhtrying=$!
-#	sleep 3
-#done
-
-ip a
+ping -c 1 $shareip
+if [ $? -ne 0 ]; then
+	echo -e "\n Failed. \n"
+	sudo bash /etc/profile.d/launcher.sh
+fi
 
 echo "done" >> /script/logs.txt
 
@@ -37,15 +34,15 @@ echo "model=\"$model\"" > restore.cfg
 echo "machine name: $model" >> /script/logs.txt
 
 echo "get path ... " >> /script/logs.txt
-#path=$(cat /mnt/smb/Utils/path.txt|grep "$model"|sed 's/.*;//g'|sed 's/\r//g')
-#path=$(cat /mnt/smb/Utils$db_path|grep "$model"|sed 's/.*;//g'|sed 's/\r//g')
+
 path=$(cat /mnt/smb/Utils$db_path|grep "$model;"|sed 's/.*;//g'|sed 's/\r//g')
 echo "path=\"$path\"" >> restore.cfg
 echo "path=$path" >> /script/logs.txt
-if [ $path = "" ]
+if [ $path = ]
 then
 	echo "/!\\ there is no path for $model in path.txt /!\\" >> /script/logs.txt
-	whiptail --title "Error Message" --msgbox "there is no path for $model in path.txt" 8 78
+	whiptail --title "Error Message" --msgbox "there is no path for \"$model\" in path.txt" 8 78
+	sudo bash /script/launch.sh
 else
 	echo "path = $path" >> /script/logs.txt
 	echo "mounting clonezilla files:" >> /script/logs.txt
@@ -54,6 +51,5 @@ else
 	sudo mount -t cifs "//$shareip$path" /home/partimag -o username=guest,password=""
 	echo "done" >> /script/logs.txt
 	echo "launch choose disk:" >> /script/logs.txt
-	sleep 1.5
 	sudo bash /script/choosedisk.sh
 fi
